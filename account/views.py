62c1b4deef4759808a3c5504a0acc42362fb4a42
caroline_esther <carolineeupanggabean@gmail.com>
2022-12-10 15:07:28 +0700

    Adding views

--- account/views.py
+++ account/views.py
@@ -170,3 +170,7 @@ def profile_json(request):
     user = request.user
     data = User.objects.filter(username=user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_username(request, username):
+    data = User.objects.filter(username=username)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
